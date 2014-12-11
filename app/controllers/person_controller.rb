2b9bd59679a1887a9a47197444e8a90c66e305a5
Vader@sith <gustavosouzabanegas@gmail.com>
2014-12-10 23:26:11 -0200

    add

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,4 +1,22 @@
 class PersonController < ApplicationController
+    def index
+        @person = Person.all
+    end
+
+    def show
+        @person = Person.find(params[:id])
+    end
+
     def new
     end
+
+    def create
+        @person = Person.new(params[:person])
+        @person.save
+        redirect_to @person
+    end
+    private
+        def article_params
+            params.require(:person).permit(:name, :last_name, :email, :company,:job_title, :phone, :website)
+        end
 end
